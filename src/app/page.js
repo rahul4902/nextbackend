0a186bdfb0c260bae12ac16f15a86306dbee7ff6
rahul.cyber <rahul.cyber2023@outlook.com>
2024-07-24 19:44:21 +0530

    d

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-    <h1>8888</h1>
+    <h1>88889999</h1>
     </main>
   );
 }
